fb98deece987d5031bf84bf21ae224d5e8e0b129
fatihserta <fatihserta@gmail.com>
2025-04-17 13:20:43 +0000

    Deploying to gh-pages from @ fatihserta/fatihserta.github.io@df4f23eda648a4bff4092952a0c983f05b4dda4c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,9 +412,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-environmental-impact-of-cold-seeps-in-biogeochemical-cycles",
+          title: 'Environmental impact of cold seeps in biogeochemical cycles',
+          description: "How does seeping methane influence the water column ecosystems?",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
