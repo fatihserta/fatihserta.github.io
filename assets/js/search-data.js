726e2603f2a914a100bc67368668f67c2f5b82c9
fatihserta <fatihserta@gmail.com>
2025-06-06 10:41:09 +0000

    Deploying to gh-pages from @ fatihserta/fatihserta.github.io@cb1f10eca5ccb9295ebd5bd3264effaec8ad5c25 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
