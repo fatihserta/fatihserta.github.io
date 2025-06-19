a3b0a7f395b2bb636d42defff6671a22595392e4
fatihserta <fatihserta@gmail.com>
2025-06-19 11:10:38 +0000

    Deploying to gh-pages from @ fatihserta/fatihserta.github.io@3e7ee8e3e36eb4c6cb7bb87569917375537f470c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -418,6 +418,13 @@ ninja.data = [{
           window.open("mailto:%66%61%74%69%68%73%65%72%74%61@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/fatihserta", "_blank");
+        },
+      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
